8e661105353129205535d25487e95d0fc18d033c
Ionut_G <gabroveanuionut22@stud.ase.ro>
2025-05-23 23:34:48 +0300

    Factory pattern work

--- Adapter/AdvancedMediaPlayer.java
+++ Adapter/AdvancedMediaPlayer.java
@@ -6,7 +6,7 @@ class AdvancedMediaPlayer {
     }
 
     void playMp4(String fileName) {
-        System.out.println("Playing mp4 file. Name: " + fileName);
+        System.out.println("Playing mp3 file. Name: " + fileName);
     }
 }
 
